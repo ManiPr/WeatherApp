e11ad46adc88485b8262f2e48abcb5209c227cd8
ManiGeek <maniweeb@gmail.com>
2022-10-31 09:26:27 +0330

    startProject

---
+++ app.js
@@ -0,0 +1,15 @@
+
+let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+let searchBoxInput=document.querySelector('.search-box')
+let cityElem=document.querySelector('.city')
+let dateElem=document.querySelector('.date')
+let tempElem=document.querySelector('.temp')
+let weatherElem=document.querySelector('.weather')
+let hiLow=document.querySelector('.hi-low')
+
+searchBoxInput.addEventListener('keydown',()=>{
+    
+})
